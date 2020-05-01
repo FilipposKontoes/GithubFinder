13bffc94beeb1df0966f391f7c1dbc38e117231b
Filippos Kontoes <fil13kon@gmail.com>
2020-05-02 01:34:13 +0300

    Updates

---
+++ precache-manifest.346d8d8770966616f2b9184c10773d09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac77092431b9a91dc7e95c65e88dace4",
+    "url": "/GithubFinder/index.html"
+  },
+  {
+    "revision": "2b72bef165d96ea769b7",
+    "url": "/GithubFinder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "572ef17469dfd9aaaec6",
+    "url": "/GithubFinder/static/js/2.fd302e27.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/GithubFinder/static/js/2.fd302e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b72bef165d96ea769b7",
+    "url": "/GithubFinder/static/js/main.e591fbdd.chunk.js"
+  },
+  {
+    "revision": "9ceb9fddf6495ec2e6d1",
+    "url": "/GithubFinder/static/js/runtime-main.3af09acb.js"
+  }
+]);
